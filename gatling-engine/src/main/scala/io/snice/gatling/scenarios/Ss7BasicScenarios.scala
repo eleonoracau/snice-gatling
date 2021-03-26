81e0594c3e2dadfcf5c4e536141616f6c536f1f6
stwang123 <stwang@twilio.com>
2021-03-26 10:58:15 +0100

    remove unused

--- gatling-engine/src/main/scala/io/snice/gatling/scenarios/Ss7BasicScenarios.scala
+++ gatling-engine/src/main/scala/io/snice/gatling/scenarios/Ss7BasicScenarios.scala
@@ -27,11 +27,6 @@ object Ss7BasicScenarios {
 
   val ss7Attach = (simulationConfig: Ss7SimulationConfig) => scenario("Ss7 Attachment Scenario")
     .feed(feeder(simulationConfig))
-    .exec(session => {
-      val numberOfRequestVectors = (NUMBER_OF_REQUESTED_VECTORS, numberOfRequestedVectors(random.nextInt(numberOfRequestedVectors.length)))
-      val customGT = (CUSTOM_GT, simulationConfig.customGT)
-      session.setAll(numberOfRequestVectors, customGT)
-    })
 //    .pause(5.seconds)
     .exec(Ss7Requests.air)
     .pause(1.seconds)
