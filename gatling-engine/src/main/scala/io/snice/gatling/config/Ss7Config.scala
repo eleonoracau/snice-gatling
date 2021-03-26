ff5e1c51cda9ddda7f2a9346b3b7bcf247247925
stwang123 <stwang@twilio.com>
2021-03-26 10:56:43 +0100

    nit

--- gatling-engine/src/main/scala/io/snice/gatling/config/Ss7Config.scala
+++ gatling-engine/src/main/scala/io/snice/gatling/config/Ss7Config.scala
@@ -10,12 +10,19 @@ class Ss7Config(@JsonProperty("engine") _ss7EngineConfig: Ss7EngineConfig,
   val ss7SimulationConfig = checkNotNull(_ss7SimulationConfig, "Ss7 simulation configuration cannot be null")
 }
 
-class Ss7SimulationConfig(@JsonProperty("atOnceUsers") _atOnceUsers: Int,
+class Ss7SimulationConfig(@JsonProperty("imsiStart") _imsiStart: Long,
+                          @JsonProperty("imsiEnd") _imsiEnd: Long,
+                          @JsonProperty("customGT") _customGT: Long,
+                          @JsonProperty("atOnceUsers") _atOnceUsers: Int,
                           @JsonProperty("startConstantUsersPerSec") _startConstantUsersPerSec: Int,
                           @JsonProperty("rampRatePerSec") _rampRatePerSec: Int,
                           @JsonProperty("rampRateTarget") _rampRateTarget: Int,
                           @JsonProperty("endConstantUsersPerSec") _endConstantUsersPerSec: Int,
                           @JsonProperty("intervalInMinutes") _intervalInMinutes: Int) {
+
+  val imsiStart = checkNotNull(_imsiStart, "imsiStart cannot be null")
+  val imsiEnd = checkNotNull(_imsiEnd, "imsiEnd cannot be null")
+  val customGT = checkNotNull(_customGT, "customGT cannot be null")
   val atOnceUsers = checkNotNull(_atOnceUsers, "atOnceUsers cannot be null")
   val startConstantUsersPerSec = checkNotNull(_startConstantUsersPerSec, "startConstantUsersPerSec cannot be null")
   val rampRatePerSec = checkNotNull(_rampRatePerSec, "rampRatePerSec cannot be null")
