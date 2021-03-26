e51670c14003b19027fc2178ebe553b8460fbddb
stwang123 <stwang@twilio.com>
2021-03-26 10:56:30 +0100

    imsi range

--- gatling-engine/src/main/scala/io/snice/gatling/scenarios/Ss7BasicScenarios.scala
+++ gatling-engine/src/main/scala/io/snice/gatling/scenarios/Ss7BasicScenarios.scala
@@ -1,21 +1,32 @@
 package io.snice.gatling.scenarios
 
 import io.gatling.core.Predef._
-import io.snice.gatling.config.{Ss7Config, Ss7SimulationConfig}
+import io.snice.gatling.config.Ss7SimulationConfig
 import io.snice.gatling.requests.Ss7Requests
 import io.snice.gatling.requests.Ss7Requests.{CUSTOM_GT, NUMBER_OF_REQUESTED_VECTORS}
 
 import scala.concurrent.duration._
 import scala.util.Random
+import scala.collection.immutable._
 
 object Ss7BasicScenarios {
 
-  val feeder = csv("data/imsis.csv").random.circular
   val numberOfRequestedVectors = List.apply(1, 2, 3, 5)
   val random = new Random
 
+  val feeder = (simulationConfig: Ss7SimulationConfig) => {
+    val feederArray = (simulationConfig.imsiStart to simulationConfig.imsiEnd).map {imsi =>
+      Map(
+        "imsi" -> imsi.toString,
+        NUMBER_OF_REQUESTED_VECTORS -> numberOfRequestedVectors(random.nextInt(numberOfRequestedVectors.length)),
+        CUSTOM_GT -> simulationConfig.customGT
+      )
+    }.toArray
+    array2FeederBuilder(feederArray)
+  }
+
   val ss7Attach = (simulationConfig: Ss7SimulationConfig) => scenario("Ss7 Attachment Scenario")
-    .feed(feeder)
+    .feed(feeder(simulationConfig))
     .exec(session => {
       val numberOfRequestVectors = (NUMBER_OF_REQUESTED_VECTORS, numberOfRequestedVectors(random.nextInt(numberOfRequestedVectors.length)))
       val customGT = (CUSTOM_GT, simulationConfig.customGT)
